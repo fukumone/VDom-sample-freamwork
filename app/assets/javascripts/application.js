58f9260e17c9fc8deaa0f1118f9313c4c7548013
t-fukui <fukuimone@gmail.com>
2015-11-06 21:14:45 +0900

    React関連処理 componentsフォルダに格納

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,5 @@
 //= require jquery_ujs
 
 window.onload = function() {
-  React = require('react')
-  ReactDom = require('react-dom')
-  require('./articles.es6')
+  require('./components/articles.es6')
 }
--- app/assets/javascripts/components/articles.es6
+++ app/assets/javascripts/components/articles.es6
@@ -1,3 +1,5 @@
+const React = require('react')
+const ReactDom = require('react-dom')
 var h1Tag = React.createElement('h1', null, 'Hoge')
 
 ReactDom.render(
