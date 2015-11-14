0e0ab5e99b3a96c8e4f14ea4701d8c511628606d
t-fukui <fukuimone@gmail.com>
2015-11-14 17:01:26 +0900

    editの処理追加:途中

--- react-sample/app/assets/javascripts/routes.js.jsx
+++ react-sample/app/assets/javascripts/routes.js.jsx
@@ -48,6 +48,7 @@ $(() => {
         <ReactRouter.Route path="/articles/?" handler={Articles}>
             <ReactRouter.DefaultRoute name="index" handler={ArticleIndex}/>
             <ReactRouter.Route name="new" handler={ArticleNew}/>
+            <ReactRouter.Route name="edit" handler={ArticleEdit}/>
             <ReactRouter.Route name="show" path="/articles/:id" handler={ArticleShow}/>
         </ReactRouter.Route>
     )
